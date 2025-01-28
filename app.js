14a3dae7f68802e2f1d9e380021e373e353d2d02
Seth Coleman <84861800+SethColeman50@users.noreply.github.com>
2025-01-28 15:53:04 -0500

    Add placeholder functionality to search button.
    The search button takes placeholder data and creates <p> tags on the webpage with the names of each search result. Just here as a proof of concept for now.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const server = "https://h6aj1c0336.execute-api.us-east-1.amazonaws.com/prod";
 
 getHello()
+document.getElementById("search_button").addEventListener("click", search)
 
 async function getHello() {
     try {
@@ -17,4 +18,30 @@ async function getHello() {
     } catch (error) {
         console.error(error);
     }
+}
+
+/**
+ * Gets data for and changes front end for search.
+ */
+function search() {
+    const data = getSearchData()
+    const results_box = document.getElementById("results")
+    results_box.innerHTML = '' // clears the results box from previous searches
+
+    for (element of data) {
+        const result = document.createElement("p")
+        result.innerText = element.name
+        results_box.appendChild(result)
+    }
+}
+
+/**
+ * Gets data for search from API. Currently a placeholder.
+ * @returns a list of objects representing search results.
+ */
+function getSearchData() {
+    return [
+        { name: "test1" },
+        { name: "test2" },
+    ]
 }

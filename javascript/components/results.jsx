a2e1c64ec3d60d8475f89f03cc77f3c0456453d6
Trevor Gray <trevormgray@gmail.com>
2025-04-04 09:41:48 -0400

    Revert results.jsx

--- javascript/components/results.jsx
+++ javascript/components/results.jsx
@@ -3,27 +3,20 @@ import React, { useEffect, useState, useRef } from "react";
 import "/styles/results.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-const ResultsSection = ({ initialResults }) => {
-  const [results, setResults] = useState(initialResults);
-  const [currentPage, setCurrentPage] = useState(initialResults.currentPage);
+const ResultsSection = ({ results }) => {
+  const [isVisible, setIsVisible] = useState(false);
   const resultsRef = useRef(null);
 
   useEffect(() => {
     if (results.dockets.length > 0) {
+      setIsVisible(true);
+
       resultsRef.current?.scrollIntoView({ behavior: "smooth", block: "start" });
     }
   }, [results]);
 
-  useEffect(() => {
-    // Fetch new data when `currentPage` changes
-    fetch(`/api/search-results?page=${currentPage}`)
-      .then(response => response.json())
-      .then(data => setResults(data))
-      .catch(error => console.error("Error fetching data:", error));
-  }, [currentPage]);
-
   return (
-    <div ref={resultsRef} className={`results-container mt-4`}>
+    <div ref={resultsRef} className={`results-container mt-4 ${isVisible ? "fade-in" : ""}`}>
       <h2 className="results-title">Search Results</h2>
       {results.dockets.map((docket, index) => (
         <div key={index} className="result-item border p-3 mb-2 rounded">
@@ -39,7 +32,7 @@ const ResultsSection = ({ initialResults }) => {
           <p><strong>Date Modified:</strong> {new Date(docket.dateModified).toLocaleDateString()}</p>
         </div>
       ))}
-      <PageSwitcher current_page={currentPage} total_pages={results.totalPages} setPage={setCurrentPage} />
+      <PageSwitcher current_page={results.currentPage} total_pages={results.totalPages}/>
     </div>
   );
 };
